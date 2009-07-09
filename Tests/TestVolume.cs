0d88453fc2baa74435c3becbc59ea49f26d4fba6
Christopher James Halse Rogers <raof@ubuntu.com>
2009-07-09 14:40:36 +1000

    Add the start of a Volume implementation

---
+++ Tests/TestVolume.cs
@@ -0,0 +1,36 @@
+//  
+//  Copyright © 2009 Christopher James Halse Rogers <raof@ubuntu.com>
+// 
+//  TestVolume.cs is a part of Pulseaudio#
+// 
+//  Pulseaudio# is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  Pulseaudio# is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with Pulseaudio#.  If not, see <http://www.gnu.org/licenses/>.
+// 
+
+using System;
+using NUnit.Framework;
+
+namespace Pulseaudio
+{
+    [TestFixture()]
+    public class TestVolume
+    {       
+        [Test()]
+        public void TestResetSetsToNorm()
+        {
+            Volume v = new Volume ();
+            v.Reset ();
+            Assert.AreEqual (Volume.Norm, v);
+        }
+    }
+}
