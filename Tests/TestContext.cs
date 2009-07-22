53ecc8bc479b534409cee7d570f3f4a32f0798db
Christopher James Halse Rogers <raof@ubuntu.com>
2009-07-23 08:18:37 +1000

    Add test for non-empty SinkInfo.Name

--- Tests/TestContext.cs
+++ Tests/TestContext.cs
@@ -152,5 +152,24 @@ namespace Pulseaudio
             };
             RunUntilEventSignal (c.Connect, callback_called, "Timeout waiting for EnumerateSinks");
         }
+
+        [Test()]
+        public void SinkInfoContainsNonEmptyName ()
+        {
+            var callback_called = new EventWaitHandle (false, EventResetMode.AutoReset);
+            Context c = new Context ();
+            c.Ready += delegate {
+                c.EnumerateSinks ((SinkInfo info, int eol) => {
+                    if (eol == 0) {
+                        Assert.IsNotNull (info, "Sink info is null");
+                        Assert.IsNotNull (info.Name, "info.Name is null");
+                        Assert.IsNotEmpty (info.Name, "info.Name is empty");
+                    } else {
+                        callback_called.Set ();
+                    }
+                });
+            };
+            RunUntilEventSignal (c.Connect, callback_called, "Timeout waiting for EnumerateSinks");
+        }
     }
 }
